3718e92df226088ddd6f38976c2b927cea5efd57
Maza92 <maza05599@gmail.com>
2025-04-27 17:18:22 +0000

    feat(api): enhance Swagger documentation for authentication endpoints with detailed response examples

--- src/main/java/com/auth/authservice/config/SwaggerConfig.java
+++ src/main/java/com/auth/authservice/config/SwaggerConfig.java
@@ -1,13 +1,17 @@
 package com.auth.authservice.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 @Configuration
 @OpenAPIDefinition(info = @Info(title = "Spring Autentication API", version = "1.0", description = "API for Spring Authentication"), servers = {
-        @Server(url = "/api/v1", description = "Default Server URL")
+                @Server(url = "/api/v1", description = "Default Server URL")
 })
+@SecurityScheme(name = "Auth", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", scheme = "bearer")
 public class SwaggerConfig {
 }
--- src/main/java/com/auth/authservice/controller/auth/IAuthController.java
+++ src/main/java/com/auth/authservice/controller/auth/IAuthController.java
@@ -22,8 +22,49 @@ import jakarta.validation.Valid;
 @Tag(name = "Auth", description = "Controller for authentication")
 public interface IAuthController {
 
-	@Operation(summary = "Login", description = "Authenticate user")
 	@PostMapping("/login")
+	@Operation(summary = "Login", description = "Authenticate user")
+	@ApiResponse(responseCode = "200", description = "User authenticated successfully", content = @Content(mediaType = "application/json", schema = @Schema(implementation = LoginResponseDto.class)))
+	@ApiResponse(responseCode = "400", description = "Invalid request", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiErrorDto.class), examples = @ExampleObject(name = "ValidationErrorExample", value = """
+			{
+			    "status": 400,
+				"message": "Validation error",
+				"timestamp": "2025-04-22T00:44:52.345768198Z",
+				"errors": [
+					"email: must not be blank",
+					"password: must not be blank"
+				]
+			}
+			""")))
+	@ApiResponse(responseCode = "401", description = "Invalid credentials", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ApiErrorDto.class), examples = {
+			@ExampleObject(name = "InvalidCredentialsExample", value = """
+					{
+						"status": 401,
+						"message": "Invalid credentials",
+						"timestamp": "2025-04-22T00:44:52.345768198Z",
+						"path": "/api/v1/api/auth/login",
+						"method": "POST",
+						"errors": []
+					}
+					"""), @ExampleObject(name = "UserInactiveExample", value = """
+					{
+						"status": 401,
+						"message": "User is inactive",
+						"timestamp": "2025-04-22T00:44:52.345768198Z",
+						"path": "/api/v1/api/auth/login",
+						"method": "POST",
+						"errors": []
+					}
+					"""), @ExampleObject(name = "MaxJwtCountExample", value = """
+					{
+						"status": 401,
+						"message": "The maximum number of active sessions has been reached",
+						"timestamp": "2025-04-22T00:44:52.345768198Z",
+						"path": "/api/v1/api/auth/login",
+						"method": "POST",
+						"errors": []
+					}
+					""") }))
 	ResponseEntity<LoginResponseDto> login(LoginRequestDto request);
 
 	@PostMapping("/register")
